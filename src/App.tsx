8e08266ec6c248285aa16792840fdd66560631e7
Arnav Patel <patelarnavm+github@gmail.com>
2023-02-23 11:09:59 -0500

    worked on nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React from "react";
-import Logo from "./assets/Logo.png";
+import Logo_Invert from "./assets/Logo_Invert.png";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <nav className="nav">
-        <img src={Logo} alt="Logo" />
+        <img src={Logo_Invert} alt="Logo" className="nav__logo" />
         <div>
-          <a href="">Who are we</a>
-          <a href="">What we do</a>
-          <a href="">Projects</a>
-          <a href="">Contact</a>
+          <a href="" className="nav__section">
+            Who are we
+          </a>
+          <a href="" className="nav__section">
+            What we do
+          </a>
+          <a href="" className="nav__section">
+            Projects
+          </a>
+          <a href="" className="nav__section">
+            Contact
+          </a>
         </div>
       </nav>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
+      <section className="info">Test</section>
+      <section className="services">Test</section>
+      <section className="projects">Test</section>
+      <section className="contact">Test</section>
       <footer></footer>
     </div>
   );
